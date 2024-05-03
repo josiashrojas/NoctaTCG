6f27a0c6d4329cafe6df0c129f376842709db507
josiashrojas <josiashrojas@gmail.com>
2024-05-03 17:40:56 -0400

    Modelo de las cartas creado y migrado

--- cartas/admin.py
+++ cartas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import NoctaCard
 
 # Register your models here.
+admin.site.register(NoctaCard)
--- cartas/models.py
+++ cartas/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class NoctaCard(models.Model):
+    codigo = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=200)
+    rasgo1 = models.CharField(max_length=200)
+    rasgo2 = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
+    vida = models.IntegerField()
+    mana = models.IntegerField()
+    dano = models.IntegerField()
+    skin = models.BooleanField(default=False)
+    edicion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=1000)
